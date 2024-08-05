8050f9a83fd32ff10ffcdad3b9749278cc944227
sujithramans20 <sujithramans2020@gmail.com>
2024-08-05 12:22:20 +0530

    firstcommit

---
+++ config.py
@@ -0,0 +1,80 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7058713089:AAEYCNd4uuS-mbVX6OGCmxChcuqZvZtK1Yw")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "12799559"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "077254e69d93d08357f25bb5f4504580")
+
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001938091397"))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "6698279837"))
+
+#Port
+PORT = os.environ.get("PORT", "8280")
+
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://test:test@cluster0.q9llhnj.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME","HotChoice")
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002051659890"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\n This is video sharing bot Powered by Hot Choice.")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "6698279837").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my channel Hot Choice to use me\n\nKindly Please join Channel</b>")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "")
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", True) == 'True'
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "Go to"
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(6698279837)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
